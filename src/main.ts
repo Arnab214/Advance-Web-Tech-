0d5afe076e06cc5ba6aebc6b04e304c4238e54cc
Arnab214 <ornobkabbo7@gmail.com>
2026-02-15 14:20:37 +0600

    arnab customer update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,         
+      forbidNonWhitelisted: true,
+      transform: true,         
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(3000);
+
+  console.log('swapOwner API running on http://localhost:3000/api');
 }
 bootstrap();
