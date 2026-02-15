d327903eb1484cc2787e68d41be76de7c09010eb
ALIFA-MAX <23-50170-1@student.aiub.edu>
2026-02-15 14:09:00 +0600

    Seller update

---
+++ src/seller/seller.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDTO, UpdateProfileDTO } from './seller.dto';
+ 
+@Injectable()
+export class SellerService {
+  getHello(): object {
+    return { message: 'Hello Seller!' };
+  }
+ 
+  addProduct(myobj: CreateProductDTO) {
+    return {
+      message: 'Product added successfully',
+      product: myobj,
+    };
+  }
+ 
+  getInventory(): object {
+    return {
+      message: 'Seller Inventory List',
+    };
+  }
+ 
+  updatePrice(id: string, price: number): object {
+    return {
+      message: 'Price updated successfully',
+      productId: id,
+      newPrice: price,
+    };
+  }
+ 
+  deleteItem(id: string): object {
+    return {
+      message: 'Item deleted successfully',
+      productId: id,
+    };
+  }
+ 
+  addDiscount(id: string, discount: number): object {
+    return {
+      message: 'Discount added successfully',
+      productId: id,
+      discount: discount + '%',
+    };
+  }
+ 
+  getReviews(): object {
+    return {
+      message: 'Product Reviews List',
+    };
+  }
+ 
+  // 9️⃣ Update Profile (PUT /seller/profile)
+  updateProfile(myobj: UpdateProfileDTO) {
+    return {
+      message: 'Profile updated successfully',
+      profile: myobj,
+    };
+  }
+}
+ 
